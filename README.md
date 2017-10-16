44c150f3b9489da1926867c88e9484d60ea809d6
Christopher Shea <cmshea@gmail.com>
2017-10-16 14:14:20 -0400

    Add xkcd-title.user.js

--- README.md
+++ README.md
@@ -6,7 +6,10 @@ and find useful.
 ## Scripts
 
 * [dinosaur-helper.user.js][dinosaur-helper.user.js]: Adds the comic's
-title text and the contact email subject line to the page on
-qwantz.com.
+  title text and the contact email subject line to the page on
+  qwantz.com.
+* [xkcd-title.user.js][xkcd-title.user.js]: Adds the comic's title
+  text on xkcd.com.
 
 [dinosaur-helper.user.js]: https://www.kilosecond.com/userscripts/dinosaur-helper.user.js
+[xkcd-title.user.js]: https://www.kilosecond.com/userscripts/xkcd-title.user.js
---
+++ xkcd-title.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         XKCD Title Text
+// @namespace    https://kilosecond.com/
+// @homepage     https://github.com/tie-rack/userscripts
+// @version      0.1
+// @description  Show the XKCD title text
+// @author       tie-rack
+// @match        *://xkcd.com/*
+// @grant        none
+// @license      MPL 2.0; https://www.mozilla.org/en-US/MPL/2.0/
+// ==/UserScript==
+
+(() => {
+  'use strict';
+
+  const css = `
+.kilosecond--subtitle {
+  background-color: #FFFFCC;
+  border: 1px solid yellow;
+  font-variant: normal;
+  margin: 10px 20px;
+  padding: 1em;
+}
+`;
+
+  const attachCSS = () => {
+    const style = document.createElement('style');
+    style.textContent = css;
+
+    document.head.appendChild(style);
+  };
+
+  const createSubtitle = (parent, subtitle) => {
+    const div = document.createElement('div');
+    div.className = 'kilosecond--subtitle';
+    div.textContent = subtitle;
+
+    parent.appendChild(div);
+  };
+
+  const comic = document.querySelector('div#comic img[title]');
+
+  if (comic) {
+    const parent = comic.parentElement;
+
+    const title = comic.title;
+
+    attachCSS();
+    createSubtitle(parent, title);
+  }
+})();
